61ef0f5dccbc384d69957b9f5c40f9d201c24981
kev7862 <kevmatite7862@gmail.com>
2017-09-11 13:02:09 +0300

    Created a data WeatherHttpClient class, which is the Heart of the application since we're parsing the JSONObject here

---
+++ app/src/main/java/data/WeatherHttpClient.java
@@ -0,0 +1,10 @@
+package data;
+
+/**
+ * Created by matite on 9/11/17.
+ */
+
+// This is the Heart of Application,because we're parsing/getting the JSONObject here.
+public class WeatherHttpClient {
+
+}
